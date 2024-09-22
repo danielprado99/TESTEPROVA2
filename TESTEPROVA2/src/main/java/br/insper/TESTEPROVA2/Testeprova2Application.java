8abb88897afe4452616e51d44c207bd175003079
Daniel Prado <daniel.vascomadagama2@gmail.com>
2024-09-22 20:54:31 -0300

    teste

---
+++ TESTEPROVA2/src/main/java/br/insper/TESTEPROVA2/Testeprova2Application.java
@@ -0,0 +1,13 @@
+package br.insper.TESTEPROVA2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testeprova2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testeprova2Application.class, args);
+	}
+
+}
---
+++ TESTEPROVA2/src/test/java/br/insper/TESTEPROVA2/Testeprova2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.TESTEPROVA2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testeprova2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
