f7184aef8dbd0088ada7e98e9a348c2eee1e3789
Daniel Prado <daniel.vascomadagama2@gmail.com>
2024-11-11 10:54:28 -0300

    prova api 2

--- src/main/java/br/insper/TESTEPROVA2/TokenService.java
+++ src/main/java/br/insper/TESTEPROVA2/TokenService.java
@@ -1,20 +1,18 @@
 package br.insper.TESTEPROVA2;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class TokenService {
 
-    @Value("${auth.validation.url}")
-    private String validationUrl;
+    private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
+    private static final String VALIDATION_URL = "http://184.72.80.215/usuario/validate";
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
@@ -27,19 +25,27 @@ public class TokenService {
     public boolean validateTokenAndRole(String token, String requiredRole) {
         try {
             ResponseEntity<String> response = restTemplate.exchange(
-                    validationUrl,
+                    VALIDATION_URL,
                     HttpMethod.GET,
                     createHttpEntity(token),
                     String.class
             );
 
-            if (response.getBody() != null) {
+            if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
                 JsonNode jsonNode = objectMapper.readTree(response.getBody());
                 String userRole = jsonNode.path("role").asText();
-                return userRole.equals(requiredRole);
+                logger.info("User role: {}, Required role: {}", userRole, requiredRole);
+
+                if ("ADMIN".equals(userRole)) {
+                    return true; // ADMIN tem acesso a tudo
+                } else if ("DEVELOPER".equals(userRole) && ("DEVELOPER".equals(requiredRole) || "GET".equals(requiredRole))) {
+                    return true; // DEVELOPER tem acesso apenas para operações GET
+                }
             }
+            logger.warn("Token validation failed or insufficient permissions");
             return false;
         } catch (Exception e) {
+            logger.error("Error validating token: ", e);
             return false;
         }
     }
