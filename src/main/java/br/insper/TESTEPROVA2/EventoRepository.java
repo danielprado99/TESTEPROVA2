3f1cdf59201085dce5ce375f270c89939bebbf9e
Daniel Prado <daniel.vascomadagama2@gmail.com>
2024-11-11 10:44:00 -0300

    prova api

--- src/main/java/br/insper/TESTEPROVA2/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.insper.TESTEPROVA2;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface EventoRepository extends MongoRepository<Evento, String> {
-    List<Evento> findByNomeContaining(String nome);  // Para buscar eventos por nome
-}
